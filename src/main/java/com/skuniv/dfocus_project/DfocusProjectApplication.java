4b64a132d599b813c5c632adf64c7d802cbdb3ed
cochae <ycw1203@naver.com>
2025-10-20 15:36:40 +0900

    Dfocus Project initial setting

---
+++ src/main/java/com/skuniv/dfocus_project/DfocusProjectApplication.java
@@ -0,0 +1,13 @@
+package com.skuniv.dfocus_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfocusProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DfocusProjectApplication.class, args);
+    }
+
+}
